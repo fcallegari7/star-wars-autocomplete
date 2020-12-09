c981b35a138af50c14b3f5c06f0437b1864fe472
fcallegari7 <fabiocallegari.it@gmail.com>
2020-12-08 16:45:15 -0800

    add Cancel Request

--- src/fetch-character-epic.js
+++ src/fetch-character-epic.js
@@ -1,6 +1,6 @@
 import { ajax } from 'rxjs/ajax';
 import { ofType } from 'redux-observable';
-import { map, mergeMap } from 'rxjs/operators';
+import { map, mergeMap, takeUntil } from 'rxjs/operators';
 import { FETCH_CHARACTERS, fetchCharactersFulfilled } from './actions';
 
 const ENDPOINT = 'http://star-wars-characters.glitch.me/api/search/';
@@ -10,8 +10,13 @@ const fetchCharactersEpic = (action$) => {
     ofType(FETCH_CHARACTERS),
     mergeMap((action) =>
       ajax.getJSON(ENDPOINT + action.payload.searchTerm).pipe(
-        tap((value) => console.log(value)),
         map((response) => fetchCharactersFulfilled(response.results)),
+        takeUntil(
+          action$.pipe(
+            tap((value) => console.log('CANCELLING!', value)),
+            ofType(FETCH_CHARACTERS),
+          ),
+        ),
       ),
     ),
   );
